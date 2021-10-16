1ec1c723503c66311da2ed96d2ec2ef37858b296
Jakub Styn <s22449@pjwstk.edu.pl>
2021-10-16 12:25:49 +0200

    first commit

---
+++ demo (2)/demo/src/main/java/com/pjatkway/demo/JstynApplication.java
@@ -0,0 +1,13 @@
+package com.pjatkway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstynApplication.class, args);
+	}
+
+}
